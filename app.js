26ceb9993ad0d207ac91e5062a057257701d8b3a
Harshil <harshilkarir72@gmail.com>
2023-11-22 16:33:12 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const passwordBox = document.getElementById("password");
+const generateBtn = document.getElementById("btn");
+
+let length = 12;
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxy";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+-=,.";
+const allChars = upperCase + lowerCase + numbers + symbols;
+
+function createPassword() {
+  let password = " ";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += numbers[Math.floor(Math.random() * numbers.length)];
+  password += symbols[Math.floor(Math.random() * symbols.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+  passwordBox.value = password;
+}
+function copyPassword() {
+  passwordBox.select();
+  document.execCommand("copy");
+}
